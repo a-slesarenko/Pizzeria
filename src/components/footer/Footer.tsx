5fed06201878a0b5f072232c4726967055b5f889
a-slesarenko <andreysveltov@gmail.com>
2024-08-04 22:29:17 +0300

    footer reworked again, fix pizzablock title bug, fix filters menu bug

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,28 +6,40 @@ const Footer = () => {
         <footer>
             <div className="container">
                 <ul className={styles.list}>
-                    <li className={styles.left}>
-                        <h2>Карьера:</h2>
-                        <a href="mailto:slesarenko.1994@inbox.ru">
-                            AP_career@gmail.com
-                        </a>
-                        <p>
-                            ОАО «Легендарные Технологии»
-                            Юридический адрес: 194331, г. Москва, пер. Гагаринский, д. 3
-                        </p>
+                    <li className={styles.career_block}>
+                        <div  className={styles.career_link}>
+                            <h2>Карьера:</h2>
+                            <a href="mailto:slesarenko.1994@inbox.ru">
+                                AP_career@gmail.com
+                            </a>
+                        </div>
+                        <div className={styles.career_text}>
+                            <p>
+                                ОАО «Легендарные Технологии»
+                                Юридический адрес: 194331, г. Москва, пер. Гагаринский, д. 3
+                            </p>
+                        </div>
                     </li>
-                    <li className={styles.center}>
-                        <span>Pizzeria Andrey's Pizza</span>
-                        <p>Быстрая доставка!</p>
-                        <p>© 2024 All rights reserved</p>
+                    <li className={styles.copyrights_block}>
+                        <div className={styles.copyrights_delivery}>
+                            <span>Pizzeria Andrey's Pizza</span>
+                            <p>Быстрая доставка!</p>
+                        </div>
+                        <div className={styles.copyright}>
+                            <p>© 2024 All rights reserved</p>
+                        </div>
                     </li>
-                    <li className={styles.right}>
-                    <p>Почта для связи:</p>
-                        <a href="mailto:slesarenko.1994@inbox.ru">
-                            AndreysPizza@gmail.com
-                        </a>
-                        <p>Телефон:</p>
-                        <a href="tel:+79044327169">8-800-222-04-05</a>
+                    <li className={styles.contacts_block}>
+                        <div className={styles.mail}>
+                            <p>Почта для связи:</p>
+                            <a href="mailto:slesarenko.1994@inbox.ru">
+                                AndreysPizza@gmail.com
+                            </a>
+                        </div>
+                        <div className={styles.phone}>
+                            <p>Телефон:</p>
+                            <a href="tel:+79044327169">8-800-222-04-05</a>
+                        </div>
                     </li>
                 </ul>
             </div>
